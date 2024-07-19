dcb2419510c07db67548db95a5fb70ed6c943b0f
Gabriel <gabriel.luthun@gmail.com>
2024-07-19 09:24:06 +0200

    feat(Program.cs): add database connection program

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using MySql.Data.MySqlClient;
+static void consoleConnexion()
+{
+    MySqlConnection connexionBDD = new MySqlConnection("server=localhost;user id=root;database= mcd_and_mld_into_bdd_exercice;password=");
+
+    try
+    {
+        connexionBDD.Open();
+        Console.WriteLine("Connexion à la base de données réussie");
+
+    //Récupérer la valeur de id_culture dans la table culture
+        MySqlCommand cmd2 = connexionBDD.CreateCommand();
+        cmd2.CommandText = "SELECT id_culture FROM culture";
+        MySqlDataReader reader = cmd2.ExecuteReader();  
+        
+        //Afficher la valeur de id_culture
+        while (reader.Read())
+        {
+            Console.WriteLine(reader["id_culture"]);
+        }
+       
+    } catch (Exception ex)
+    {
+        Console.WriteLine("Erreur de connexion à la base de données : " + ex.Message);
+        throw;
+    }
+};
+
+consoleConnexion();
+
