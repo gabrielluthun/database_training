18f9ac0b5b0a96f3c7149aa6c226c5b44e53710d
Gabriel <gabriel.luthun@gmail.com>
2024-07-24 12:54:46 +0200

    feat: add function for listening database and handling requests

--- Program.cs
+++ Program.cs
@@ -1,30 +1,47 @@
-﻿using MySql.Data.MySqlClient;
-static void consoleConnexion()
+﻿using GestionAgricole;
+using GestionAgricole.App.Database;
+using MySql.Data.MySqlClient;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+// HttpListener : classe .NET permettant de communiquer via les protocoles HTTP
+
+#region Création du serveur
+HttpListener listener = new HttpListener();
+listener.Prefixes.Add("http://localhost:8080/");
+listener.Start();
+Console.WriteLine("Serveur démarré sur http://localhost:8080/");
+#endregion
+
+#region Méthodes de gestion des requêtes
+// Boucle infinie pour écouter les requêtes
+while (true)
 {
-    MySqlConnection connexionBDD = new MySqlConnection("server=localhost;user id=root;database= mcd_and_mld_into_bdd_exercice;password=");
+    //Contexte : objet qui contient toutes les informations sur la requête (réponse, requête)
+    HttpListenerContext context = listener.GetContext();
+    ThreadPool.QueueUserWorkItem(objet => RequestHandler(context));
 
-    try
-    {
-        connexionBDD.Open();
-        Console.WriteLine("Connexion à la base de données réussie");
+}
 
-    //Récupérer la valeur de id_culture dans la table culture
-        MySqlCommand cmd2 = connexionBDD.CreateCommand();
-        cmd2.CommandText = "SELECT id_culture FROM culture";
-        MySqlDataReader reader = cmd2.ExecuteReader();  
-        
-        //Afficher la valeur de id_culture
-        while (reader.Read())
-        {
-            Console.WriteLine(reader["id_culture"]);
-        }
-       
-    } catch (Exception ex)
-    {
-        Console.WriteLine("Erreur de connexion à la base de données : " + ex.Message);
-        throw;
-    }
-};
 
-consoleConnexion();
+//Fonction statique pour récupérer une requête
+static void RequestHandler(HttpListenerContext context)
+{
+    HttpListenerRequest request = context.Request;
+    Console.WriteLine($"Requête reçue :  {request.Url} ");
 
+    //Récupérer les données de la base de données
+    Db database = Db.GetDataBase();
+
+    //Sérialiser le Db.Query en JSON
+    object o = new {
+        Cultures = Culture.CultureReader(),
+        Parcelles = Parcelle.ParcelleReader()
+    };
+    string jsonResponse = JsonSerializer.Serialize(o);
+    byte[] responsesBytes = Encoding.UTF8.GetBytes(jsonResponse);
+    context.Response.ContentType = "application/json";
+    context.Response.OutputStream.Write(responsesBytes, 0, responsesBytes.Length);
+    context.Response.OutputStream.Close();
+}
+#endregion
