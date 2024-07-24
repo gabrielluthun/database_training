93e9adddf29188a1956f9c9e84d73d4b0bad4277
Gabriel <gabriel.luthun@gmail.com>
2024-07-24 16:38:57 +0200

    feat(Program.cs): add some endpoints for the URL

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿using GestionAgricole;
 using GestionAgricole.App.Database;
-using MySql.Data.MySqlClient;
 using System.Net;
 using System.Text;
 using System.Text.Json;
@@ -28,18 +27,54 @@ while (true)
 static void RequestHandler(HttpListenerContext context)
 {
     HttpListenerRequest request = context.Request;
-    Console.WriteLine($"Requête reçue :  {request.Url} ");
+    Console.WriteLine($"Requête reçue :  {request.Url} Route : {request.Url.AbsolutePath}");
+
+    //Initialisation de la réponse par défaut
+   var data = new { message = "Endpoint non trouvé" };
+
+    //Vérifier l'URL pour déterminer l'endpoint
+    switch (request.Url.AbsolutePath)
+    {
+        case "/culture":
+            //Afficher les données de la BDD de la table "culture"
+            List<Culture> cultures = Culture.CultureReader();
+            string cultureJson = JsonSerializer.Serialize(cultures);
+            data = new { message = cultureJson };
+            break;
+        case "/parcelle":
+            List<Parcelle> parcelles = Parcelle.ParcelleReader();
+            string parcelleJson = JsonSerializer.Serialize(parcelles);
+            data = new { message = parcelleJson };
+            break;
+        case "/engrais":
+            List<Engrais> engrais = Engrais.EngraisReader();
+            string engraisJson = JsonSerializer.Serialize(engrais);
+            data = new { message = engraisJson };
+            break;
+        case "/unités":
+            data = new { message = "Je poucave pas ma réponse" };
+            break;
+    }
+
+
+
+    string jsonData = JsonSerializer.Serialize(data);
+    byte[] responseBytes = Encoding.UTF8.GetBytes(jsonData);
+
 
     //Récupérer les données de la base de données
     Db database = Db.GetDataBase();
 
-    //Sérialiser le Db.Query en JSON
+    //Sérialiser les BDD en JSON
     object o = new {
         Cultures = Culture.CultureReader(),
-        Parcelles = Parcelle.ParcelleReader()
+        Parcelles = Parcelle.ParcelleReader(),
+        Engrais = Engrais.EngraisReader(),
+        Unités = Unité.UniteReader(),
+        ElementChimiques = ElementChimiques.ECReader()
     };
-    string jsonResponse = JsonSerializer.Serialize(o);
-    byte[] responsesBytes = Encoding.UTF8.GetBytes(jsonResponse);
+    //string jsonResponse = JsonSerializer.Serialize(o);
+    byte[] responsesBytes = Encoding.UTF8.GetBytes(jsonData);
     context.Response.ContentType = "application/json";
     context.Response.OutputStream.Write(responsesBytes, 0, responsesBytes.Length);
     context.Response.OutputStream.Close();
