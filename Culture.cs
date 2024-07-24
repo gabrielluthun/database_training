e6baac125ef65737ba051b9f64fa7aa47ce048f3
Gabriel <gabriel.luthun@gmail.com>
2024-07-24 12:50:20 +0200

    feat:successful connection to an external database and reading the data

--- Culture.cs
+++ Culture.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MySql.Data.MySqlClient;
+using GestionAgricole.App.Database;
 
 namespace GestionAgricole
 {
@@ -14,28 +11,52 @@ namespace GestionAgricole
         private double numeroParcelle;
         private int codeProduction;
         private double coordonnées;
-        private DateOnly dateDebut;
-        private DateOnly dateFin;
-        private double quantiteRecoltee;
+        private DateTime dateDebut;
+        private DateTime dateFin;
+        private decimal quantiteRecoltee;
 
-        public int numeroIdentifiantCulture { get => identifiantCulture; set => identifiantCulture = value; }
+        public int NumeroIdentifiantCulture { get => identifiantCulture; set => identifiantCulture = value; }
         public double NumeroDeParcelle { get => numeroParcelle; set => numeroParcelle = value; }
         public int CodeDeProduction { get => codeProduction; set => codeProduction = value; }
         public double Coordonnées{ get => coordonnées; set => coordonnées = value; }
-        public DateOnly DateDeDebut { get => dateDebut; set => dateDebut = value; }
-        public DateOnly DateDeFin { get => dateFin; set => dateFin = value; }
-        public double QuantiteRecoltee { get => quantiteRecoltee; set => quantiteRecoltee = value; }
+        public DateTime DateDeDebut { get => dateDebut; set => dateDebut = value; }
+        public DateTime DateDeFin { get => dateFin; set => dateFin = value; }
+        public decimal QuantiteRecoltee { get => quantiteRecoltee; set => quantiteRecoltee = value; }
 
-        public Culture(int identifiantCulture, double numeroParcelle, int codeProduction, double coordonnées, DateOnly dateDebut, DateOnly dateFin, double quantiteRecoltee)
+
+        public static List<Culture> CultureReader()
         {
-            //On met 'this' pour faire référence aux attributs de la classe
-            this.identifiantCulture = identifiantCulture;
-            this.numeroParcelle = numeroParcelle;
-            this.codeProduction = codeProduction;
-            this.coordonnées = coordonnées;
-            this.dateDebut = dateDebut;
-            this.dateFin = dateFin;
-            this.quantiteRecoltee = quantiteRecoltee;
+            Db db = Db.GetDataBase();
+            string query = "SELECT * FROM Culture";
+            List<Culture> result = new List<Culture>();
+            try
+            {
+                db.Connection.Open();
+                MySqlCommand cmd = new MySqlCommand(query, db.Connection);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    result.Add(new Culture
+                    {
+                        NumeroIdentifiantCulture = reader.GetInt32("identifiant_culture"),
+                        DateDeDebut = reader.GetDateTime("date_debut"),
+                        DateDeFin = reader.GetDateTime("date_fin"),
+                        QuantiteRecoltee = reader.GetDecimal("qt_recolte"),
+                        NumeroDeParcelle = reader.GetInt16("no_parcelle"),
+                        CodeDeProduction = reader.GetInt16("code_production"),
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Source + " : " + ex.Message);
+            }
+            finally
+            {
+                db.Connection.Close();
+            }
+            return result;
         }
         #endregion
     }
